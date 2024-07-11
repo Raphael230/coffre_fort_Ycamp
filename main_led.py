bbffb363c02714b8410ca16183f2af6b0115837d
praphael <raphael.pidjot1@gmail.com>
2024-07-11 20:35:14 +0200

    rien a changé normalement

--- main_led.py
+++ main_led.py
@@ -6,33 +6,21 @@
 from machine import Pin # Classe pour gérer les broches GPIO
 from time import sleep_ms # Classe pour temporiser
 
-# La LED est assignéeà la broche D4
 vert = Pin(23,  mode=Pin.OUT)
-rouge = Pin(21, mode=Pin.OUT)
-jaune = Pin(19, mode=Pin.OUT)
-
-# while True :
-# 	sleep_ms(500) # Pose 0.5 seconde
-# 	led.off()
-# 	print("LED éteinte")
-# 	sleep_ms(500) # Pause 0.5 seconde
-# 	led.on() 
-# 	print("LED allumée")
-
+rouge = Pin(19, mode=Pin.OUT)
+jaune = Pin(21, mode=Pin.OUT)
 
 def clignotement_vert ():
 	vert.on()
 	sleep_ms(500) # Pose 0.5 seconde
 	vert.off()
-	sleep_ms(500)
-	print("NICE")
+	sleep_ms(1500)
 
 def clignotement_rouge():
 	rouge.on()
 	sleep_ms(500) # Pose 0.5 seconde
 	rouge.off()
-	sleep_ms(500)
-	print("ERROR")
+	sleep_ms(1500)
 
 def clignotement_jaune():
 	jaune.on()
